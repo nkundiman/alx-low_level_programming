b904277d36a512f7551708c90e4b2af3e2c42a8f
nkundiman <augunkundimana@gmail.com>
2023-02-22 17:08:16 -0800

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,36 @@
-#include "main.h"
+#ifndef MAIN_H_INCLUDED
 
-/**
- * main - prints _putchar and a new line
- * Return: always 0
- */
-int main(void)
-{
-    putchar('');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+#define MAIN_H_INCLUDED
 
-    return(0);
+
+
+int _putchar(char c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int n);
+
+int print_last_digit(int n);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+
+
+#endif
+
